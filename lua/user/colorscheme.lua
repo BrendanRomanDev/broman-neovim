6e6bb0db2ed4ebdf3965c49a5ba34e1f378747cb
BrendanRomanDev <your.username@thriveworks.com>
2022-12-12 14:14:26 -0500

    Theme change + misc

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,8 @@
-local colorscheme = "sonokai"
+-- local colorscheme = "sonokai"
+-- local colorscheme = "base16-nord"
+local colorscheme = "base16-onedark"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
