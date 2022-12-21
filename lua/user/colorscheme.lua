c5df4df7b09fc56cf4e0566269e607e5881a72dd
BrendanRomanDev <your.username@thriveworks.com>
2022-12-21 10:13:59 -0500

    Colorscheme & nvim-tree right align

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,5 @@
 -- local colorscheme = "sonokai"
--- local colorscheme = "base16-nord"
-local colorscheme = "base16-onedark"
+local colorscheme = "base16-nord"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
