0fc7c4f83964c6d08db6d17d4a42a1f1ce12411d
BrendanRomanDev <roman.bp15@gmail.com>
2023-12-21 13:17:54 -0500

    Solarized Osaka

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,10 +1,49 @@
 -- local colorscheme = "sonokai"
-local colorscheme = "base16-nord"
+-- local colorscheme = "base16-nord"
 
+-- setup for solarized-osaka
+require("solarized-osaka").setup({
+	-- your configuration comes here
+	-- or leave it empty to use the default settings
+	transparent = false, -- Enable this to disable setting the background color
+	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+	styles = {
+		-- Style to be applied to different syntax groups
+		-- Value is any valid attr-list value for `:help nvim_set_hl`
+		comments = { italic = true },
+		keywords = { italic = true },
+		functions = {},
+		variables = {},
+		-- Background styles. Can be "dark", "transparent" or "normal"
+		sidebars = "transparent", -- style for sidebars, see below
+		floats = "transparent", -- style for floating windows
+	},
+	sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+	day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+	hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+	dim_inactive = false, -- dims inactive windows
+	lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+
+	--- You can override specific color groups to use other groups or a hex color
+	--- function will be called with a ColorScheme table
+	---@param colors ColorScheme
+	on_colors = function(colors) end,
+
+	--- You can override specific highlights to use other groups or a hex color
+	--- function will be called with a Highlights and ColorScheme table
+	---@param highlights Highlights
+	---@param colors ColorScheme
+	on_highlights = function(hl, c)
+		hl.Normal = { guibg = c.transparent }
+		hl.NormalNC = { guibg = c.transparent }
+		hl.TelescopeNormal = { bg = c.transparent }
+		hl.TelescopePromptNormal = { bg = c.transparent }
+		hl.TelescopeTitle = { bg = c.transparent }
+		hl.TelescopeBorder = { bg = c.transparent }
+	end,
+})
+local colorscheme = "solarized-osaka"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	return
 end
-
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
-vim.cmd("hi NormalNC guibg=NONE")
